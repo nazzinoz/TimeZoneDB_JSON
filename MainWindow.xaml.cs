1729bfc9d534a344b2aa87b6000209949fa7a5ff
noz1995 <tranhoan95@yahoo.com.vn>
2020-03-12 23:35:08 +0700

    Full features

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,11 +1,4 @@
-﻿using Microsoft.VisualBasic.FileIO;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.IO.Compression;
-using System.Linq;
-using System.Text.Json;
+﻿using System;
 using System.Windows;
 
 namespace TimeZoneDB_JSON
@@ -22,127 +15,154 @@ namespace TimeZoneDB_JSON
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            if (StaticValues.MAIN_WINDOW == null)
-                StaticValues.MAIN_WINDOW = this;
+            if (Worker.MAIN_WINDOW == null)
+                Worker.MAIN_WINDOW = this;
         }
 
         private void Button_CheckUpdate_Click(object sender, RoutedEventArgs e)
         {
-            if (!File.Exists(StaticValues.DOWNLOAD_VERSION_FILE))
+            var currentTitle = this.Title;
+            this.IsEnabled = false;
+            this.Title = currentTitle + Worker.TITLE_CHECKING;
+            this.InvalidateVisual();
+            try
             {
-                MessageBox.Show(StaticValues.MESSAGE_NO_DB);
-                return;
+                var updateInfo = Worker.CheckUpdate();
+                if (updateInfo == null)
+                {
+                    MessageBox.Show(Worker.MESSAGE_NO_DB);
+                    return;
+                }
+
+                var localVersion = updateInfo.Item1;
+                var currentVersion = updateInfo.Item2;
+
+                var message = Worker.MESSAGE_LOCAL_VERSION + localVersion + Environment.NewLine
+                              + Worker.MESSAGE_CURRENT_VERSION + currentVersion + Environment.NewLine
+                              + (currentVersion.CompareTo(localVersion) > 0 ? Worker.MESSAGE_REQUIRE_REDOWNLOADING : Worker.MESSAGE_UP_TO_DATE);
+
+                MessageBox.Show(message);
             }
-
-            var localVersion = File.ReadAllText(StaticValues.DOWNLOAD_VERSION_FILE);
-
-            StaticValues.WEB_CLIENT.Headers.Add("User-Agent", StaticValues.DEVELOPER);
-            var currentVersion = StaticValues.WEB_CLIENT.DownloadString(StaticValues.VERSION_API);
-
-            var message = StaticValues.MESSAGE_LOCAL_VERSION + localVersion
-                + Environment.NewLine + StaticValues.MESSAGE_CURRENT_VERSION + currentVersion
-                + Environment.NewLine +
-            (currentVersion.CompareTo(localVersion) > 0 ? StaticValues.MESSAGE_REQUIRE_REDOWNLOADING : StaticValues.MESSAGE_UP_TO_DATE);
-
-            MessageBox.Show(message);
+            catch
+            {
+                MessageBox.Show(Worker.MESSAGE_FAILED);
+            }
+            this.IsEnabled = true;
+            this.Title = currentTitle;
         }
 
         private void Button_Download_Click(object sender, RoutedEventArgs e)
         {
-            if (!Directory.Exists(StaticValues.DOWNLOAD_PATH))
-                Directory.CreateDirectory(StaticValues.DOWNLOAD_PATH);
+            var currentTitle = this.Title;
+            this.IsEnabled = false;
+            this.Title = currentTitle + Worker.TITLE_DOWNLOADING;
+            try
+            {
+                var message = Worker.MESSAGE_CURRENT_VERSION + Worker.Download() + Environment.NewLine
+                              + Worker.MESSAGE_DOWNLOADED;
 
-            StaticValues.WEB_CLIENT.Headers.Add("User-Agent", StaticValues.DEVELOPER);
-            var currentVersion = StaticValues.WEB_CLIENT.DownloadString(StaticValues.VERSION_API);
-
-            StaticValues.WEB_CLIENT.Headers.Add("User-Agent", StaticValues.DEVELOPER);
-            StaticValues.WEB_CLIENT.DownloadFile(StaticValues.DOWNLOAD_API, StaticValues.DOWNLOAD_TIMEZONE_FILE);
-
-            File.WriteAllText(StaticValues.DOWNLOAD_VERSION_FILE, currentVersion);
-
-            var message = StaticValues.MESSAGE_CURRENT_VERSION + currentVersion
-                + Environment.NewLine + StaticValues.MESSAGE_DOWNLOADED;
-
-            MessageBox.Show(message);
+                MessageBox.Show(message);
+            }
+            catch
+            {
+                MessageBox.Show(Worker.MESSAGE_FAILED);
+            }
+            this.IsEnabled = true;
+            this.Title = currentTitle;
         }
 
         private void Button_Extract_Click(object sender, RoutedEventArgs e)
         {
-            ZipFile.ExtractToDirectory(StaticValues.DOWNLOAD_TIMEZONE_FILE, StaticValues.EXTRACT_PATH, true);
-
-            MessageBox.Show(StaticValues.MESSAGE_EXTRACTED);
+            var currentTitle = this.Title;
+            this.IsEnabled = false;
+            this.Title = currentTitle + Worker.TITLE_EXTRACTING;
+            try
+            {
+                Worker.Extract();
+                MessageBox.Show(Worker.MESSAGE_EXTRACTED);
+            }
+            catch
+            {
+                MessageBox.Show(Worker.MESSAGE_FAILED);
+            }
+            this.IsEnabled = true;
+            this.Title = currentTitle;
         }
 
         private void Button_Generate_Click(object sender, RoutedEventArgs e)
         {
-            var zoneParser = new TextFieldParser(StaticValues.EXTRACTED_ZONE);
-            zoneParser.SetDelimiters(",");
-
-            var zoneParsed = new Dictionary<string, Dictionary<string, dynamic>>();
-            while (!zoneParser.EndOfData)
+            var currentTitle = this.Title;
+            this.IsEnabled = false;
+            this.Title = currentTitle + Worker.TITLE_GENERATING;
+            try
             {
-                var lineData = zoneParser.ReadFields();
-
-                var timezoneId = int.Parse(lineData[0]);
-                var countryCode = lineData[1];
-                var name = lineData[2];
-
-                zoneParsed[timezoneId.ToString()] = new Dictionary<string, dynamic> { { StaticValues.KEY_COUNTRY_CODE, countryCode }, { StaticValues.KEY_NAME, name } };
+                Worker.Generate();
+                MessageBox.Show(Worker.MESSAGE_GENERATED);
             }
-
-            var countryParser = new TextFieldParser(StaticValues.EXTRACTED_COUNTRY);
-            countryParser.SetDelimiters(",");
-
-            var countryParsed = new Dictionary<string, string>();
-            while (!countryParser.EndOfData)
+            catch
             {
-                var lineData = countryParser.ReadFields();
-
-                var countryCode = lineData[0];
-                var countryName = lineData[1];
-
-                countryParsed[countryCode] = countryName;
+                MessageBox.Show(Worker.MESSAGE_FAILED);
             }
-            countryParser.Close();
-
-            var timezoneParser = new TextFieldParser(StaticValues.EXTRACTED_TIMEZONE);
-            timezoneParser.SetDelimiters(",");
-
-            var timezoneParsed = new Dictionary<string, List<Dictionary<string, dynamic>>>();
-            while (!timezoneParser.EndOfData)
-            {
-                var lineData = timezoneParser.ReadFields();
-
-                var timezoneId = int.Parse(lineData[0]);
-                var abbreviation = lineData[1];
-                var unixTimestamp = long.Parse(lineData[2]);
-                var offset = int.Parse(lineData[3]);
-                var isDaylightSavingTime = int.Parse(lineData[4]) == 1;
-
-                if (!timezoneParsed.ContainsKey(timezoneId.ToString()))
-                    timezoneParsed[timezoneId.ToString()] = new List<Dictionary<string, dynamic>>();
-
-                timezoneParsed[timezoneId.ToString()].Add(new Dictionary<string, dynamic> { { StaticValues.KEY_ABBREVIATION, abbreviation }, { StaticValues.KEY_UNIX_TIME_STAMP, unixTimestamp }, { StaticValues.KEY_OFFSET, offset }, { StaticValues.KEY_DST, isDaylightSavingTime } });
-            }
-            timezoneParser.Close();
-
-            var filledData = new Dictionary<string, Dictionary<string, dynamic>>(zoneParsed);
-            timezoneParsed.Keys.ToList().ForEach(key => filledData[key][StaticValues.KEY_CHANGE] = timezoneParsed[key]);
-
-            if (!Directory.Exists(StaticValues.RESULT_PATH))
-                Directory.CreateDirectory(StaticValues.RESULT_PATH);
-
-            File.WriteAllText(StaticValues.RESULT_ZONE, JsonSerializer.Serialize(zoneParsed));
-            File.WriteAllText(StaticValues.RESULT_COUNTRY, JsonSerializer.Serialize(countryParsed));
-            File.WriteAllText(StaticValues.RESULT_TIMEZONE, JsonSerializer.Serialize(timezoneParsed));
-            File.WriteAllText(StaticValues.RESULT_FILE, JsonSerializer.Serialize(filledData));
-
-            MessageBox.Show(StaticValues.MESSAGE_GENERATED);
+            this.IsEnabled = true;
+            this.Title = currentTitle;
         }
 
         private void Button_Open_Click(object sender, RoutedEventArgs e)
         {
-            Process.Start("explorer.exe", StaticValues.RESULT_PATH);
+            var currentTitle = this.Title;
+            this.IsEnabled = false;
+            this.Title = currentTitle + Worker.TITLE_OPENING;
+            try
+            {
+                Worker.OpenResult();
+            }
+            catch
+            {
+                MessageBox.Show(Worker.MESSAGE_FAILED);
+            }
+            this.IsEnabled = true;
+            this.Title = currentTitle;
+        }
+
+        private void Button_AllInOne_Click(object sender, RoutedEventArgs e)
+        {
+            var currentTitle = this.Title;
+            this.IsEnabled = false;
+            try
+            {
+                this.Title = currentTitle + Worker.TITLE_DOWNLOADING;
+                Worker.Download();
+                this.Title = currentTitle + Worker.TITLE_EXTRACTING;
+                Worker.Extract();
+                this.Title = currentTitle + Worker.TITLE_GENERATING;
+                Worker.Generate();
+                this.Title = currentTitle + Worker.TITLE_OPENING;
+                Worker.OpenResult();
+            }
+            catch
+            {
+                MessageBox.Show(Worker.MESSAGE_FAILED);
+            }
+            this.IsEnabled = true;
+            this.Title = currentTitle;
+        }
+
+        private void Button_ClearCache_Click(object sender, RoutedEventArgs e)
+        {
+            var currentTitle = this.Title;
+            this.IsEnabled = false;
+            this.Title = currentTitle + Worker.TITLE_DELETING;
+            try
+            {
+                Worker.ClearCache();
+                MessageBox.Show(Worker.MESSAGE_DELETED);
+            }
+            catch
+            {
+                MessageBox.Show(Worker.MESSAGE_FAILED);
+            }
+            this.IsEnabled = true;
+            this.Title = currentTitle;
         }
     }
 }
---
+++ Worker.cs
@@ -0,0 +1,184 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+
+namespace TimeZoneDB_JSON
+{
+    public static class Worker
+    {
+
+        public const string DEVELOPER = "nazzinoz";
+        public const string APP_NAME = "TimeZoneDB_JSON";
+        public const string VERSION_API = "https://timezonedb.com/date.txt";
+        public const string DOWNLOAD_API = "https://timezonedb.com/files/timezonedb.csv.zip";
+
+        public const string MESSAGE_NO_DB = "No databases downloaded.";
+        public const string MESSAGE_LOCAL_VERSION = "Local version: ";
+        public const string MESSAGE_CURRENT_VERSION = "Current version: ";
+        public const string MESSAGE_UP_TO_DATE = "Up to date.";
+        public const string MESSAGE_REQUIRE_REDOWNLOADING = "Require redownloading.";
+        public const string MESSAGE_DOWNLOADED = "Downloaded.";
+        public const string MESSAGE_EXTRACTED = "Extracted.";
+        public const string MESSAGE_GENERATED = "Generated.";
+        public const string MESSAGE_DELETED = "Deleted.";
+        public const string MESSAGE_FAILED = "Failed.";
+
+        public const string TITLE_CHECKING = " - Checking...";
+        public const string TITLE_DOWNLOADING = " - Downloading...";
+        public const string TITLE_EXTRACTING = " - Extracting...";
+        public const string TITLE_GENERATING = " - Generating...";
+        public const string TITLE_OPENING = " - Opening...";
+        public const string TITLE_DELETING = " - Deleting...";
+
+        public const string KEY_COUNTRY_CODE = "countryCode";
+        public const string KEY_NAME = "name";
+        public const string KEY_ABBREVIATION = "abbreviation";
+        public const string KEY_UNIX_TIME_STAMP = "unixTimestamp";
+        public const string KEY_OFFSET = "offset";
+        public const string KEY_DST = "dst";
+
+        public const string KEY_ZONE = "zone";
+        public const string KEY_COUNTRY = "country";
+        public const string KEY_TIMEZONE = "timezone";
+
+        public const string KEY_CHANGE = "change";
+
+        public static readonly string EXECUTE_PATH = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DEVELOPER, APP_NAME);
+
+        public static readonly string DOWNLOAD_PATH = Path.Combine(EXECUTE_PATH, "download");
+        public static readonly string EXTRACT_PATH = Path.Combine(EXECUTE_PATH, "extract");
+        public static readonly string RESULT_PATH = Path.Combine(EXECUTE_PATH, "result");
+
+        public static readonly string DOWNLOAD_VERSION_FILE = Path.Combine(DOWNLOAD_PATH, "version.txt");
+        public static readonly string DOWNLOAD_TIMEZONE_FILE = Path.Combine(DOWNLOAD_PATH, "timezonedb.csv.zip");
+
+        public static readonly string EXTRACTED_ZONE = Path.Combine(EXTRACT_PATH, KEY_ZONE + ".csv");
+        public static readonly string EXTRACTED_COUNTRY = Path.Combine(EXTRACT_PATH, KEY_COUNTRY + ".csv");
+        public static readonly string EXTRACTED_TIMEZONE = Path.Combine(EXTRACT_PATH, KEY_TIMEZONE + ".csv");
+
+        public static readonly string RESULT_ZONE = Path.Combine(RESULT_PATH, KEY_ZONE + ".json");
+        public static readonly string RESULT_COUNTRY = Path.Combine(RESULT_PATH, KEY_COUNTRY + ".json");
+        public static readonly string RESULT_TIMEZONE = Path.Combine(RESULT_PATH, KEY_TIMEZONE + ".json");
+
+        public static readonly string RESULT_FILE = Path.Combine(RESULT_PATH, "timezoneList.json");
+
+        public static readonly WebClient WEB_CLIENT = new WebClient();
+        public static MainWindow MAIN_WINDOW;
+
+        public static Tuple<string, string> CheckUpdate()
+        {
+            if (!File.Exists(DOWNLOAD_VERSION_FILE))
+                return null;
+
+            var localVersion = File.ReadAllText(DOWNLOAD_VERSION_FILE);
+
+            WEB_CLIENT.Headers.Add("User-Agent", DEVELOPER);
+            var currentVersion = WEB_CLIENT.DownloadString(VERSION_API);
+
+            return new Tuple<string, string>(localVersion, currentVersion);
+        }
+
+        public static string Download()
+        {
+            if (!Directory.Exists(DOWNLOAD_PATH))
+                Directory.CreateDirectory(DOWNLOAD_PATH);
+
+            WEB_CLIENT.Headers.Add("User-Agent", DEVELOPER);
+            var currentVersion = WEB_CLIENT.DownloadString(VERSION_API);
+
+            WEB_CLIENT.Headers.Add("User-Agent", DEVELOPER);
+            WEB_CLIENT.DownloadFile(DOWNLOAD_API, DOWNLOAD_TIMEZONE_FILE);
+
+            File.WriteAllText(DOWNLOAD_VERSION_FILE, currentVersion);
+
+            return currentVersion;
+        }
+
+        public static void Extract()
+        {
+            ZipFile.ExtractToDirectory(DOWNLOAD_TIMEZONE_FILE, EXTRACT_PATH, true);
+        }
+
+        public static void Generate()
+        {
+            var zoneParser = new TextFieldParser(EXTRACTED_ZONE);
+            zoneParser.SetDelimiters(",");
+
+            var zoneParsed = new Dictionary<string, Dictionary<string, dynamic>>();
+            while (!zoneParser.EndOfData)
+            {
+                var lineData = zoneParser.ReadFields();
+
+                var timezoneId = int.Parse(lineData[0]);
+                var countryCode = lineData[1];
+                var name = lineData[2];
+
+                zoneParsed[timezoneId.ToString()] = new Dictionary<string, dynamic> { { KEY_COUNTRY_CODE, countryCode }, { KEY_NAME, name } };
+            }
+            zoneParser.Close();
+
+            var countryParser = new TextFieldParser(EXTRACTED_COUNTRY);
+            countryParser.SetDelimiters(",");
+
+            var countryParsed = new Dictionary<string, string>();
+            while (!countryParser.EndOfData)
+            {
+                var lineData = countryParser.ReadFields();
+
+                var countryCode = lineData[0];
+                var countryName = lineData[1];
+
+                countryParsed[countryCode] = countryName;
+            }
+            countryParser.Close();
+
+            var timezoneParser = new TextFieldParser(EXTRACTED_TIMEZONE);
+            timezoneParser.SetDelimiters(",");
+
+            var timezoneParsed = new Dictionary<string, List<Dictionary<string, dynamic>>>();
+            while (!timezoneParser.EndOfData)
+            {
+                var lineData = timezoneParser.ReadFields();
+
+                var timezoneId = int.Parse(lineData[0]);
+                var abbreviation = lineData[1];
+                var unixTimestamp = long.Parse(lineData[2]);
+                var offset = int.Parse(lineData[3]);
+                var isDaylightSavingTime = int.Parse(lineData[4]) == 1;
+
+                if (!timezoneParsed.ContainsKey(timezoneId.ToString()))
+                    timezoneParsed[timezoneId.ToString()] = new List<Dictionary<string, dynamic>>();
+
+                timezoneParsed[timezoneId.ToString()].Add(new Dictionary<string, dynamic> { { KEY_ABBREVIATION, abbreviation }, { KEY_UNIX_TIME_STAMP, unixTimestamp }, { KEY_OFFSET, offset }, { KEY_DST, isDaylightSavingTime } });
+            }
+            timezoneParser.Close();
+
+            var filledData = new Dictionary<string, Dictionary<string, dynamic>>(zoneParsed);
+            timezoneParsed.Keys.ToList().ForEach(key => filledData[key][KEY_CHANGE] = timezoneParsed[key]);
+
+            if (!Directory.Exists(RESULT_PATH))
+                Directory.CreateDirectory(RESULT_PATH);
+
+            File.WriteAllText(RESULT_ZONE, JsonSerializer.Serialize(zoneParsed));
+            File.WriteAllText(RESULT_COUNTRY, JsonSerializer.Serialize(countryParsed));
+            File.WriteAllText(RESULT_TIMEZONE, JsonSerializer.Serialize(timezoneParsed));
+            File.WriteAllText(RESULT_FILE, JsonSerializer.Serialize(filledData));
+        }
+
+        public static void OpenResult()
+        {
+            Process.Start("explorer.exe", Worker.RESULT_PATH);
+        }
+
+        public static void ClearCache()
+        {
+            Directory.Delete(Worker.EXECUTE_PATH, true);
+        }
+    }
+}
